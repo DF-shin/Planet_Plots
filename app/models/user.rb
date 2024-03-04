05c690af7d03d7b4ce2086e62a8d78459232b706
Yuefei Dong <yuefeidong124@gmail.com>
2024-03-04 16:58:51 -0500

    add reference to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :plots
+  has_many :requests
+  has_many :planets, through: :plots
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
