f0bdd1f3782e52be4295c59302694513de726e54
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-03-05 16:36:35 -0500

    flow data for plots from seed to controller to view / Add price to plot

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,24 +1,31 @@
 class PlotsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
-  before_action :set_plot, only: %i[show new create]
+  before_action :set_plot, only: [:show]
   def show
+  end
+
+  def new
     @plot = Plot.new
   end
 
   def create
     @plot = Plot.new(plot_params)
     @plot.user = current_user
-    @plot.save
-    redirect_to plot_path(@plot)
+    if @plot.save
+      redirect_to plot_path(@plot), notice: 'Plot was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_plot
-    @plot = Plot.find(params[:plot_id])
+    @plot = Plot.find_by(id: params[:id])
+    redirect_to(root_url, alert: "Plot not found.") unless @plot
   end
 
-  def review_params
-    params.require(:plot).permit(:name, :description)
+  def plot_params
+    params.require(:plot).permit(:name, :description, :planet_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,66 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 planets_name = %w[Mercury Venus Earth Mars Jupiter Saturn Uranus]
-planets_description = ["The swifted planet.", "Earth's superhead twin sister.", "Our homeland.", "The red planet.", "King of the planets.", "Jewel of the solar system.", "The original ice giant."]
+planets_description = [
+  "The swifted planet.",
+  "Earth's superhead twin sister.",
+  "Our homeland.",
+  "The red planet.",
+  "King of the planets.",
+  "Jewel of the solar system.",
+  "The original ice giant."
+]
 
 planets_name.each_with_index do |planet_name, index|
-  Planet.create!(name: planet_name, description: planets_description[index])
+  Planet.find_or_create_by!(name: planet_name, description: planets_description[index])
+end
+
+# Create a User
+user1 = User.find_or_create_by!(email: 'laurent@hackers.com') do |user_create|
+  user_create.password = 'password'
+  user_create.first_name = 'Laurent'
+  user_create.last_name = 'Lefebvre'
+end
+User.find_or_create_by!(email: 'declan@hackers.com') do |user_create|
+  user_create.password = 'password'
+  user_create.first_name = 'Declan'
+  user_create.last_name = 'Schindler'
+end
+User.find_or_create_by!(email: 'yuefei@hackers.com') do |user_create|
+  user_create.password = 'password'
+  user_create.first_name = 'Yuefei'
+  user_create.last_name = 'Dong'
+end
+
+# Assuming planets are already created
+plots_name = %w[Crater Mountain LakeHouse Desert RainForest RockyArea Garden]
+plots_description = [
+  "A deep, bowl-shaped depression formed by meteorite or asteroid impacts.",
+  "A large natural elevation of the earth's surface rising abruptly from the surrounding level.",
+  "A serene house by the lake, ideal for relaxation.",
+  "A barren, arid area of land with little to no vegetation.",
+  "A dense forest that receives high amounts of rainfall.",
+  "An area covered with rocky terrain, challenging for adventurers.",
+  "A small piece of ground, planted with flowers, herbs, vegetables, or small fruits."
+]
+
+# Ensure we have the planets created
+planets_name.each_with_index do |planet_name, index|
+  Planet.find_or_create_by!(name: planet_name, description: planets_description[index])
+end
+
+# Now, associate plots with planets and the user
+planets = Planet.all
+plots_name.each_with_index do |plot_name, index|
+  plot_description = plots_description[index]
+  planet = planets[index % planets.size] # Cycle through planets
+
+  # Create plots, associating them with a planet and the user
+  Plot.find_or_create_by!(
+    name: plot_name,
+    description: plot_description,
+    price: rand(1000.00..10_000.00),
+    planet_id: planet.id,
+    user_id: user1.id # Assign the created user's ID here
+  )
 end
