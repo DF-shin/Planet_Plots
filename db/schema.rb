d4cfb24626536e3c5e91aa88e3977cb5b3983087
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-03-05 13:20:18 -0500

    add first and last name to users, create the route for plots show,new,create, made the controller for these, made the view for shows and new

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_210812) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_173948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name"
+    t.string "img"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plots", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.bigint "planet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["planet_id"], name: "index_plots_on_planet_id"
+    t.index ["user_id"], name: "index_plots_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "plot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plot_id"], name: "index_requests_on_plot_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +51,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_210812) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "plots", "planets"
+  add_foreign_key "plots", "users"
+  add_foreign_key "requests", "plots"
+  add_foreign_key "requests", "users"
 end
