98825b183bdb332aec67740175674b895fa40731
Yuefei Dong <yuefeidong124@gmail.com>
2024-03-05 15:23:34 -0500

    simple login/logout and request-index

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,26 @@
 class RequestsController < ApplicationController
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.save
+    redirect_to requests_path
+  end
+
+  def index
+    @user = current_user
+    if @user
+      @requests = @user.requests
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:status)
+  end
 end
