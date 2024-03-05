763ffcb60aff2e99d54e807becf35d833fa4c999
Yuefei Dong <yuefeidong124@gmail.com>
2024-03-05 12:52:13 -0500

    Safety commit

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,21 @@
 class RequestsController < ApplicationController
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.save
+    redirect_to requests_path
+  end
+
+  def index
+    @requests = Request.all
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :requests, only: %i[new create index]
 end
