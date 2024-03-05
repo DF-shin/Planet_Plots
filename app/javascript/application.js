23b27ed36554a20b5d618cca256bd4ceecf2797b
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-03-05 17:58:46 -0500

    added select2 for drop down menu and give the option to users to select a planet when creating a plot

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require select2
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import $ from "jquery"
+import "select2"
+
+document.addEventListener("DOMContentLoaded", () => {
+  $(document).ready(function() {
+    $('.select2-enable').select2({
+      width: '100%', // Adjust based on your needs
+      placeholder: "Type or select a planet",
+      allowClear: true
+    });
+  });
+});
