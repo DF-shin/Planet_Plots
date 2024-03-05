6740e1e5a3c5454fb0ca75f0d9868c5e0ed4998a
Yuefei Dong <yuefeidong124@gmail.com>
2024-03-05 11:45:33 -0500

    remove N:N with planets

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :plots
   has_many :requests
-  has_many :planets, through: :plots
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
